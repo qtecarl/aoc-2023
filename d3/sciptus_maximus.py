1cee79189184f81cba6c305b2627b9208aef0370
Carl <carl@qte.se>
2023-12-04 12:01:17 +0100

    Day 3 p2

--- d3/sciptus_maximus.py
+++ d3/sciptus_maximus.py
@@ -3,34 +3,80 @@ import re
 with open ("input.txt", "r") as f:
     lines = f.readlines()
     x = 0
+    dic = {}
     for idx, line in enumerate(lines):
         res = re.finditer(r"[0-9]+", line)
         for r in res:
             surr = ""
             # Top row
+            tr = ""
             if idx > 0:
                 if r.start() == 0:
-                    surr += lines[idx-1][r.start():r.end()+1]
+                    tr = lines[idx-1][r.start():r.end()+1]
                 elif r.end() == len(line)-1:
-                    surr += lines[idx-1][r.end():r.end()]
+                    tr =  lines[idx-1][r.end():r.end()]
                 else:
-                    surr += lines[idx-1][r.start()-1:r.end()+1]
-            # Right and left
+                    tr =  lines[idx-1][r.start()-1:r.end()+1]
+                surr += tr
+                gears = re.finditer(r"\*", tr)
+                for gear in gears:
+                    row = idx
+                    col = gear.start() + r.start()
+                    if r.start() == 0:
+                        col += 1
+                    if (row, col) in dic:
+                        dic[(row, col)].append(r.group())
+                    else:
+                        dic[(row, col)] = [r.group()]
+            # Middle row
+            mr = ""
             if r.start() == 0:
-                surr += line[r.end()]
+                mr = line[r.end()]
             elif r.end() == len(line)-1:
-                surr += line[r.start()-1]
+                mr = line[r.start()-1]
             else:
-                surr += line[r.start()-1] + line[r.end()]
+                mr = line[r.start()-1] + line[r.end()]
+            surr += mr
+            gears = re.finditer(r"\*", mr)
+            for gear in gears:
+                row = idx+1
+                if len(mr) == 1:
+                    if r.start() == 0:
+                        col = r.end()
+                    elif r.end() == len(line)-1:
+                        col = r.start()+1
+                elif mr[0] == '*': 
+                    col = r.start()
+                else:
+                    col = r.end()+1
+                if (row, col) in dic:
+                    dic[(row, col)].append(r.group())
+                else:
+                    dic[(row, col)] = [r.group()]
             # Bottom row
+            br = ""
             if idx < len(lines)-1:
                 if r.start() == 0:
-                    surr += lines[idx+1][r.start():r.end()+1]
+                    br = lines[idx+1][r.start():r.end()+1]
                 elif r.end() == len(line)-1:
-                    surr += lines[idx+1][r.start()-1:r.end()]
+                    br = lines[idx+1][r.start()-1:r.end()]
                 else: 
-                    surr += lines[idx+1][r.start()-1:r.end()+1]
-
-            if surr != len(surr) * ".":
-                x += int(r.group())
+                    br = lines[idx+1][r.start()-1:r.end()+1]
+                surr += br
+                gears = re.finditer(r"\*", br)
+                for gear in gears:
+                    row = idx+2
+                    col = gear.start() + r.start()
+                    if r.start() == 0:
+                        col -= 1
+                    if (row, col) in dic:
+                        dic[(row, col)].append(r.group())
+                    else:
+                        dic[(row, col)] = [r.group()]
+    
+    for v in dic.values():
+        if len(v) == 2:
+            x += int(v[0]) * int(v[1])
+    
     print(x)
+                
